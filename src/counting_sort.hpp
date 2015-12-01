f214e6998ce3c2e07d78618c2e831973c1beff49
Bernardo Sulzbach <mafagafogigante@gmail.com>
2015-12-01 15:57:58 -0200

    Minor improvements on counting sort

--- src/counting_sort.hpp
+++ src/counting_sort.hpp
@@ -6,38 +6,43 @@
 namespace algorithms {
 
     /**
-     * Sorts a range using counting sort.
+     * Sorts the elements in the range [first, last) in nondecreasing order using counting sort. This algorithm has O(n)
+     * time complexity and O(n) space complexity. These elements must have integer type.
      *
-     * O(n) time complexity and O(n) space complexity.
+     * The order of equal elements is guaranteed to be preserved.
+     *
+     * Cannot handle more than 4,294,967,295 elements.
      */
     template<typename RandomAccessIterator, typename Value>
     void counting_sort(RandomAccessIterator begin, RandomAccessIterator end) {
         if (std::distance(begin, end) < 2) return; // Trivially sorted.
+        if (std::distance(begin, end) > 4294967295) return; // Cannot sort.
         // Find the minimum and the maximum.
         const Value &minimum = *std::min_element(begin, end);
         const Value &maximum = *std::max_element(begin, end);
-        std::vector<size_t> buckets(maximum - minimum + 1);
+        std::vector<uint32_t> vector(maximum - minimum + 1);
         RandomAccessIterator iterator = begin;
+        // Count repetitions.
         while (iterator != end) {
             // We subtract minimum from every element in order to determine which bucket it occupies.
-            buckets[*iterator - minimum]++;
+            vector[*iterator - minimum]++;
             iterator++;
         }
-        // We reuse the buckets vector to make the cumulative vector.
-        for (std::vector<size_t>::size_type i = 1; i != buckets.size(); i++) {
-            buckets[i] += buckets[i - 1];
+        // Reuse the vector to determine where each repetition ends.
+        for (std::vector<uint32_t>::size_type i = 1; i != vector.size(); i++) {
+            vector[i] += vector[i - 1];
         }
-        for (std::vector<size_t>::size_type i = 0; i != buckets.size(); i++) {
-            buckets[i]--; // Vectors are zero-indexed.
+        // As we need indices, not frequencies, subtract one from each counter.
+        for (std::vector<uint32_t>::size_type i = 0; i != vector.size(); i++) {
+            vector[i]--;
         }
-        std::vector<Value> output(begin, end);
+        std::vector<Value> output((unsigned long) std::distance(begin, end)); // Will never be negative, this is safe.
         // Iterate over the input backwards, sorting it.
         while (end != begin) {
             end--;
-            size_t index_in_cumulative_vector = *end - minimum;
+            const Value index_in_cumulative_vector = *end - minimum;
             // Also decrement the position of the next element.
-            size_t index_in_sorted_vector = buckets[index_in_cumulative_vector]--;
-            // Remember to add minimum back to our indices.
+            const Value index_in_sorted_vector = vector[index_in_cumulative_vector]--;
             output[index_in_sorted_vector] = *end;
         }
         std::copy(output.begin(), output.end(), begin);
